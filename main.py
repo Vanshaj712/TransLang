875cc28b13a42b541f58b6e3828135de05ed31e1
Vanshaj <85443589+Vanshaj712@users.noreply.github.com>
2021-10-19 11:39:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import scrolledtext
-
+import io
 
 main = tk.Tk()
 main.resizable(False,False)
@@ -31,6 +31,21 @@ ent1.place(x=480,y=310,height=25)
 
 file = None
 
+'''def trans():
+    t = translate.Translator(from_lang=tv.get(),to_lang=tv1.get())
+    t1 = t.translate(text=txt.get('1.0','end-1c'))
+    txt1['state'] = 'normal'
+    txt1.delete(1.0,tk.END)
+    txt1.insert(1.0,t1)
+    txt1['state'] = 'disabled'
+    print(t1)
+    length = len(txt.get('1.0','end-1c'))
+    if length > 500:
+        txt1['state'] = 'normal'
+        txt1.delete(1.0,tk.END)
+        txt1.insert(1.0,'CHARACTER LENGTH EXCEEDED.MAX ALLOWED CHARS : 500 CHARS')
+'''
+
 def trans():
     import googletrans
 
@@ -56,13 +71,13 @@ def save():
         else:
 
             # Save as a new file
-            f = open(file, "w")
+            f = io.open(file, "w",encoding='utf8')
             f.write(txt1.get(1.0, tk.END))
             f.close()
 
     else:
         # Save the file
-        f = open(file, "w")
+        f = io.open(file, "w",encoding='utf8')
         f.write(txt1.get(1.0, tk.END))
         f.close()
 
